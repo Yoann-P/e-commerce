59cdc71488734168313e6c1c06bd001fe250614e
Yoann Pedase <yoannpedase@MacBook-Pro-de-Yoann.local>
2024-01-11 19:12:26 +0100

    address api

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,27 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\Address;
 use App\Repository\AddressRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 
 class AccountController extends AbstractController
 {
     #[Route('/account', name: 'app_account')]
     public function index(AddressRepository $addressRepository): Response
-    {
-        $user = $this->getUser();
-        $addresses = $addressRepository->findByUser($user);
-
-        return $this->render('account/index.html.twig', [
-            'controller_name' => 'AccountController',
-            'addresses' => $addresses,
-        ]);
-    }
-
-    #[Route('/api/address', name: 'app_api_address',methods: [ 'POST'])]
-    public function postAdress(AddressRepository $addressRepository): Response
     {
         $user = $this->getUser();
         $addresses = $addressRepository->findByUser($user);
---
+++ src/Controller/Api/ApiAddressController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Controller\Api;
+
+use App\Entity\Address;
+use App\Repository\AddressRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+#[Route('/api')]
+class ApiAddressController extends AbstractController
+{
+    #[Route('/address', name: 'app_post_address', methods: ['POST'])]
+    public function index(
+        Request $req,
+        AddressRepository $addressRepository,
+        EntityManagerInterface  $manager
+    ): Response {
+        $user = $this->getUser();
+
+        if (!$user) {
+            return $this->json([
+                "isSuccess" => false,
+                "messsage" => "You are not authenticated",
+                "data" => []
+            ]);
+        }
+
+        $formData = $req->getPayload();
+
+        $address = new Address();
+        $address->setName($formData->get('name'))
+            ->setClientName($formData->get('client_name'))
+            ->setStreet($formData->get('street'))
+            ->setCodePostal($formData->get('code_postal'))
+            ->setCity($formData->get('city'))
+            ->setState($formData->get('state'))
+            ->setUser($user);
+        $manager->persist($address);
+        $manager->flush();
+
+        $addresses = $addressRepository->findByUser($user);
+
+        foreach ($addresses as $key => $address) {
+            $address->setUser(null);
+            $addresses[$key] = $address;
+        }
+
+
+        return $this->json([
+            "isSuccess" => true,
+            "data" => $addresses,
+        ]);
+    }
+}
