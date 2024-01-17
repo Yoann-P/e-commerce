6aa1d72dfc1df8c8a2659953bcc39d250af071bc
Yoann Pedase <yoannpedase@MacBook-Pro-de-Yoann.local>
2024-01-17 15:48:56 +0100

    delivery service mais pas d'affichage

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Address;
+use App\Entity\Carrier;
 use App\Entity\User;
 use App\Entity\Category;
 use App\Entity\Collections;
@@ -47,7 +48,7 @@ class DashboardController extends AbstractDashboardController
         return Dashboard::new()
             ->setTitle('E Commerce Symfo');
     }
-    
+
 
     public function configureMenuItems(): iterable
     {
@@ -58,8 +59,9 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linkToCrud('Addresses', 'fas fa-address-card', Address::class);
         yield MenuItem::linkToCrud('Pages', 'fas fa-file', Page::class);
         yield MenuItem::linkToCrud('Sliders', 'fas fa-image', Sliders::class);
+        yield MenuItem::linkToCrud('Carriers', 'fas fa-car', Carrier::class);
         yield MenuItem::linkToCrud('Collections', 'fas fa-tshirt', Collections::class);
-        yield MenuItem::linkToCrud('Settings', 'fas fa-gear', Setting::class);   
+        yield MenuItem::linkToCrud('Settings', 'fas fa-gear', Setting::class);
     }
 
     public function configureCrud(): Crud
